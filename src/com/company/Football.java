c869b56e66ebeaf374353f6c606278b7b40c5e98
Dzifa Hodey <dzifahodey@gmail.com>
2021-11-03 15:14:45 +0000

    overriding methods v1

--- src/com/company/Football.java
+++ src/com/company/Football.java
@@ -1,13 +1,14 @@
 package com.company;
 
 public class Football extends Sports{
+    private final int NUMBER_OF_TEAM_MEMBERS = 11;
     @Override
     String getName() {
-        return "Football";
+        return "football";
     }
 
     @Override
     void getNumberOfTeamMembers() {
-        super.getNumberOfTeamMembers();
+        System.out.println("Each team has " + NUMBER_OF_TEAM_MEMBERS + " players in " + getName() + ".");
     }
 }
