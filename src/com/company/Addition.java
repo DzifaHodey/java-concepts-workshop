9f0e0d611099bef5ee5d3f66d52b8c2c305e4617
Dzifa Hodey <dzifahodey@gmail.com>
2021-11-03 14:46:15 +0000

    Dzifa's overloading methods

---
+++ src/com/company/Addition.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Addition {
+
+    public void add(double a, double b){
+        System.out.println(a+b);
+    }
+
+    public void add(double a, double b, double c){
+        System.out.println(a+b+c);
+    }
+
+
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Addition ad = new Addition();
+        ad.add(1,3);
+        ad.add(1,2,3);
+        ad.add(1,3,3.5f);
+    }
+}
